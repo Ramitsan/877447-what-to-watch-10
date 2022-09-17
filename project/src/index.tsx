24fd62e4506e207305584bbbed178b1cb9dd8b6b
Ramitsan <ramitsan@gmail.com>
2022-09-17 16:00:11 +0300

    allocates movie cards a separate component and adds drawing a list of cards on the main page

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  CARDS_COUNT: 20,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App cardCount={Setting.CARDS_COUNT}/>
   </React.StrictMode>,
 );
